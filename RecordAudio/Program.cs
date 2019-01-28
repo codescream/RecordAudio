e364fa0f54d74b5e56f9451cf68e9e517424a8e1
Mark Ogilo <44981320+codescream@users.noreply.github.com>
2019-01-28 09:14:06 -0500

    Add project files.

---
+++ RecordAudio/Program.cs
@@ -0,0 +1,34 @@
+﻿using NetCoreAudio;
+using System;
+
+namespace RecordAudio
+{
+    class Program
+    {
+        /*[DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);*/
+
+        static void Main(string[] args)
+        {
+            /*mciSendString("open new Type waveaudio Alias recsound", "", 0, 0);
+            mciSendString("record recsound", "", 0, 0);
+            Console.WriteLine("recording, press Enter to stop and save ...");
+            Console.ReadLine();
+
+            mciSendString("save recsound c:\\users\\ogilo\\documents\\result.wav", "", 0, 0);
+            mciSendString("close recsound ", "", 0, 0);*/
+
+            Player player = new Player();
+            player.Record().Wait();
+            Console.WriteLine("type stop to stop recording...");
+            string stop = Console.ReadLine();
+
+            if (stop == "stop")
+            {
+                Console.WriteLine("stopping recording...");
+                player.StopRecording().Wait();
+                Console.ReadLine();
+            }
+        }
+    }
+}
